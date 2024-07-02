df5ced50d186dff1745d7cd9089a4e4779a4ce63
Nishu <nishupriya8699@gmail.com>
2024-07-02 23:19:50 +0530

    added features

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     // or display a confirmation message
     alert('Form submitted!');
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+        const links = document.querySelectorAll('.nav-bar a');
+
+        for (const link of links) {
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+
+                const targetId = this.getAttribute('href').substring(1);
+                const targetElement = document.getElementById(targetId);
+
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        }
+});
